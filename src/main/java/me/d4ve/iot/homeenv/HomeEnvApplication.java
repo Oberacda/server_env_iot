c333a1fa13ca820a3acce7d9df30736c59bd7c29
David Oberacker <david.oberacker@gmail.com>
2020-04-21 19:58:20 +0200

    Initial commit.

---
+++ src/main/java/me/d4ve/iot/homeenv/HomeEnvApplication.java
@@ -0,0 +1,13 @@
+package me.d4ve.iot.homeenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeEnvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeEnvApplication.class, args);
+    }
+
+}
