25692507419c5aa2500c32c4fd3206bfb9b34dc9
David Oberacker <david.oberacker@gmail.com>
2020-04-25 19:07:56 +0200

    Add info controller.
    Change name of IP-Address in deployment.

---
+++ src/main/java/me/d4ve/iot/homeenv/rest/api/InfoController.java
@@ -0,0 +1,19 @@
+package me.d4ve.iot.homeenv.rest.api;
+
+import me.d4ve.iot.homeenv.rest.transferobjects.InfoTransferObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    private final InfoTransferObject infoResponse;
+
+    public InfoController(InfoTransferObject infoResponse) {
+        this.infoResponse = infoResponse;
+    }
+
+    @GetMapping("/")
+    public InfoTransferObject listEndpoints() {
+        return infoResponse;
+    }
+}
